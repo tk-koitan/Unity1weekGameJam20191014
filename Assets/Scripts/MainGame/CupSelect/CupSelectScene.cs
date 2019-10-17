2be6a722cc87061f17026361c5d4fad10f223eb0
taisei <tatata0389@gmail.com>
2019-10-17 16:38:54 +0900

    コップを移動させる処理の形を作った(仮)

--- Assets/Scripts/MainGame/CupSelect/CupSelectScene.cs
+++ Assets/Scripts/MainGame/CupSelect/CupSelectScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using MainGame;
 
-// カップを選択するシーン
+// コップを選択するシーン
 
 namespace MainGame
 {
@@ -12,20 +12,22 @@ namespace MainGame
         // 初期化
         public override void Init(CommonData common_data)
         {
-            Debug.Log("カップ選択　初期化");
+            Debug.Log("コップ選択　初期化");
         }
 
         // 更新
         public override void Proc(CommonData common_data)
         {
+
+
             if (Input.GetKeyDown(KeyCode.A)) common_data.scene_queue.Enqueue("End");
-            Debug.Log("カップ選択　更新");
+            Debug.Log("コップ選択　更新");
         }
 
         // 終了
         public override void Final(CommonData common_data)
         {
-            Debug.Log("カップ選択　終了");
+            Debug.Log("コップ選択　終了");
         }
     }
 }
