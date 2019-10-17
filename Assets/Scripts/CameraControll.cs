4e27e31392c7a673979f9df9150903a0d4fcf450
tsurara <papertoad11@gmail.com>
2019-10-17 19:50:47 +0900

    カメラ修正

--- Assets/Scripts/CameraControll.cs
+++ Assets/Scripts/CameraControll.cs
@@ -8,35 +8,43 @@ public class CameraControll : MonoBehaviour
     public float rotateSpeed = 3.0f;
     public float distance = 5.0f;
     public bool ReverseVertical = false;
+    [SerializeField] float angle_x= 0;
+    [SerializeField] float angle_y = 0;
+    [SerializeField] float angle_y_min = 5;
+    [SerializeField] float angle_y_max = 60;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //入力から回転させる角度を決める
-        Vector3 angle = new Vector3(Input.GetAxis("Horizontal") * rotateSpeed, Input.GetAxis("Vertical") * rotateSpeed, 0);
         //注目する対象の位置を取得
         Vector3 pos = LookAtPoint.transform.position;
 
-        //対象の位置を中心に回転させる
-        transform.RotateAround(pos, Vector3.up, angle.x);
-        if (!ReverseVertical)
+        //入力取得
+        float input_x = Input.GetAxis("Horizontal");
+        float input_y = Input.GetAxis("Vertical");
+        if (ReverseVertical)
         {
-            transform.RotateAround(pos, transform.right, angle.y);
+            //必要なら反転
+            input_y *= -1;
         }
-        else
-        {
-            transform.RotateAround(pos, transform.right, angle.y * -1);
-        }
+
+        //回転
+        angle_x = Mathf.Repeat(angle_x - input_x * rotateSpeed, 360);
+        angle_y = Mathf.Clamp(angle_y + input_y * rotateSpeed, angle_y_min, angle_y_max);
+
+
+        //位置を調整
+        var dx = angle_x * Mathf.Deg2Rad;
+        var dy = angle_y * Mathf.Deg2Rad;
+        transform.position = new Vector3(pos.x + distance * Mathf.Sin(dy) * Mathf.Cos(dx), pos.y + distance * Mathf.Cos(dy), pos.z + distance * Mathf.Sin(dx) * Mathf.Sin(dy));
 
         //対象の方を向く
         transform.LookAt(LookAtPoint.transform);
 
-        //対象との距離を保つ
-        transform.position = (transform.position - pos).normalized * distance + pos;
     }
 }
