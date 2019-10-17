e0fce3ead57dc377721342016af6face22f1d6fc
tsurara <papertoad11@gmail.com>
2019-10-17 17:34:03 +0900

    カメラ修正

--- Assets/Scripts/CameraControll.cs
+++ Assets/Scripts/CameraControll.cs
@@ -6,6 +6,7 @@ public class CameraControll : MonoBehaviour
 {
     public GameObject LookAtPoint;
     public float rotateSpeed = 3.0f;
+    public float distance = 5.0f;
     public bool ReverseVertical = false;
     // Start is called before the first frame update
     void Start()
@@ -16,8 +17,12 @@ public class CameraControll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //入力から回転させる角度を決める
         Vector3 angle = new Vector3(Input.GetAxis("Horizontal") * rotateSpeed, Input.GetAxis("Vertical") * rotateSpeed, 0);
+        //注目する対象の位置を取得
         Vector3 pos = LookAtPoint.transform.position;
+
+        //対象の位置を中心に回転させる
         transform.RotateAround(pos, Vector3.up, angle.x);
         if (!ReverseVertical)
         {
@@ -27,5 +32,11 @@ public class CameraControll : MonoBehaviour
         {
             transform.RotateAround(pos, transform.right, angle.y * -1);
         }
+
+        //対象の方を向く
+        transform.LookAt(LookAtPoint.transform);
+
+        //対象との距離を保つ
+        transform.position = (transform.position - pos).normalized * distance + pos;
     }
 }
