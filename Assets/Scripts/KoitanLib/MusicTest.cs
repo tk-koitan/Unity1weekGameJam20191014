bf1ebf7e71e10ea0fc0a1098af6983a90acb8fca
小井田大賀 <tk_kakutasu@yahoo.co.jp>
2019-10-20 04:39:28 +0900

    BGMを再生できるようにした

---
+++ Assets/Scripts/KoitanLib/MusicTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            MusicManager.Play(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            MusicManager.Play(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            MusicManager.Play(2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            MusicManager.Play(3);
+        }
+    }
+}
