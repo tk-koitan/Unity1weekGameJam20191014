694b489409be2858cf3f6bfa2d26c5689816561d
tsurara <papertoad11@gmail.com>
2019-10-20 17:14:54 +0900

    開始時のカウントダウン

--- Assets/Scripts/GameEndScene/CallTest.cs
+++ Assets/Scripts/GameEndScene/CallTest.cs
@@ -5,10 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class CallTest : MonoBehaviour
 {
+    private ApealText appealText;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/MimiLib/GameStartText.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class GameStartText : MonoBehaviour
+{
+    TextMeshProUGUI text;
+    RectTransform rect;
+    [SerializeField] int StartTime = 180;
+
+    int count = 0;
+    Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<TextMeshProUGUI>();
+        rect = gameObject.GetComponent<RectTransform>();
+        pos = rect.transform.position;
+        text.text = "";
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count == 0)
+        {
+            DOTween.ToAlpha(() => text.color, color => text.color = color, 0f, 0f);
+            DOTween.ToAlpha(() => text.color, color => text.color = color, 1.0f, 0.5f);
+            rect.DOMoveY(pos.y + 50.0f, 1.0f);
+            text.text = "3";
+        }
+        if (count == StartTime / 3)
+        {
+            DOTween.ToAlpha(() => text.color, color => text.color = color, 0f, 0f);
+            DOTween.ToAlpha(() => text.color, color => text.color = color, 1.0f, 0.5f);
+            rect.DOMoveY(pos.y, 0f);
+            rect.DOMoveY(pos.y + 50.0f, 1.0f);
+            text.text = "2";
+        }
+        if (count == 2 * StartTime / 3)
+        {
+            DOTween.ToAlpha(() => text.color, color => text.color = color, 0f, 0f);
+            DOTween.ToAlpha(() => text.color, color => text.color = color, 1.0f, 0.5f);
+            rect.DOMoveY(pos.y, 0f);
+            rect.DOMoveY(pos.y + 50.0f, 1.0f);
+            text.text = "1";
+        }
+        if(count == StartTime)
+        {
+            DOTween.ToAlpha(() => text.color, color => text.color = color, 0f, 0f);
+            DOTween.ToAlpha(() => text.color, color => text.color = color, 1.0f, 0.5f);
+            rect.DOMoveY(pos.y, 0f);
+            rect.DOMoveY(pos.y + 50.0f, 1.0f);
+            text.text = "START!";
+            rect.DOShakePosition(1.0f,5.0f);
+        }
+        if(count == StartTime + 30)
+        {
+            DOTween.ToAlpha(() => text.color, color => text.color = color, 0f, 1f);
+        }
+
+        count++;
+    }
+}
